16670a6923ee6cc22ea89a62a6f8e05631b02180
Sehnem99 <57777514+Sehnem99@users.noreply.github.com>
2020-05-27 21:03:14 -0300

    Initial commit

---
+++ Model/cadastro.js
@@ -0,0 +1,24 @@
+var cep = document.getElementById('cadastrar');
+var cep_real = document.getElementById('nome');
+var endereco = document.getElementById('endereco');
+var cidade = document.getElementById('cidade');
+var bairro = document.getElementById('bairro');
+var estado = document.getElementById('estado');
+var httpReq = new XMLHttpRequest();
+
+
+cep.onclick = function apiCEP() {
+    httpReq.open('GET', `https://api.postmon.com.br/v1/cep/${cep_real.value}`, false);
+    httpReq.send();
+
+    var obj = httpReq.responseText;
+    var jsonOk = JSON.parse(obj);
+
+    endereco.value = jsonOk.logradouro;
+    cidade.value = jsonOk.cidade;
+    bairro.value = jsonOk.bairro;
+    estado.value = jsonOk.estado;
+
+    console.log(jsonOk);
+
+}
